debe88112411fd67d32f7cf143a6b2d19f959c8a
bagoesihsant <61548001+bagoesihsant@users.noreply.github.com>
2020-10-23 17:52:44 +0700

    Melakukan load view pada index

--- application/controllers/C_admin.php
+++ application/controllers/C_admin.php
@@ -9,6 +9,11 @@ class C_admin extends CI_Controller
     // Index
     public function index()
     {
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
         $this->load->view('admin/index');
+        $this->load->view('templates/footer_js');
+        $this->load->view('admin/custom_js');
+        $this->load->view('templates/footer');
     }
 }
