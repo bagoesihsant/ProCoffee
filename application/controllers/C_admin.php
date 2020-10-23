99edbda44527847927336fe4b1a1c42868c66f37
bagoesihsant <61548001+bagoesihsant@users.noreply.github.com>
2020-10-23 18:03:52 +0700

    Menambahkan file custom_js pada admin

--- application/controllers/C_admin.php
+++ application/controllers/C_admin.php
@@ -13,7 +13,7 @@ class C_admin extends CI_Controller
         $this->load->view('templates/v_sidebar_admin');
         $this->load->view('admin/v_dashboard');
         $this->load->view('templates/footer_js');
-        // $this->load->view('admin/custom_js');
+        $this->load->view('admin/custom_js');
         $this->load->view('templates/v_footer_admin');
     }
 }
