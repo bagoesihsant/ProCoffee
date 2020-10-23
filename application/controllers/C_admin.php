9e6fa819e223bd151f45a8186d94fb054741f418
bagoesihsant <61548001+bagoesihsant@users.noreply.github.com>
2020-10-23 18:02:24 +0700

    Perbaikan load view di Index C_admin

--- application/controllers/C_admin.php
+++ application/controllers/C_admin.php
@@ -9,11 +9,11 @@ class C_admin extends CI_Controller
     // Index
     public function index()
     {
-        $this->load->view('templates/header');
-        $this->load->view('templates/sidebar');
-        $this->load->view('admin/index');
+        $this->load->view('templates/v_header_admin');
+        $this->load->view('templates/v_sidebar_admin');
+        $this->load->view('admin/v_dashboard');
         $this->load->view('templates/footer_js');
-        $this->load->view('admin/custom_js');
-        $this->load->view('templates/footer');
+        // $this->load->view('admin/custom_js');
+        $this->load->view('templates/v_footer_admin');
     }
 }
