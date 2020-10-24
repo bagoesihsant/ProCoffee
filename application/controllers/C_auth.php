06b8f691af4abb01fae37c394fad75aade3ebaee
afakihfaj <afakihfaj@gmail.com>
2020-10-25 04:22:46 +0700

    Menambah view lupa password admin side

---
+++ application/controllers/C_auth.php
@@ -0,0 +1,28 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class C_auth extends CI_Controller
+{
+
+
+    // Index
+    public function index()
+    {
+        $data['title'] = 'Login Page';
+        $this->load->view('login_template/v_auth_header', $data);
+
+        $this->load->view('auth/v_login');
+
+        $this->load->view('login_template/v_auth_footer');
+    }
+
+    // forgotpw
+    public function fw()
+    {
+        $data['title'] = 'Forgot Password';
+        $this->load->view('login_template/v_auth_header', $data);
+        $this->load->view('auth/v_forgot-password');
+        $this->load->view('login_template/v_auth_footer');
+    }
+}
