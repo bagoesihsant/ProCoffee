dd62f9e0fa550e9f10a62483c4aace2cb52f2e72
bagoesihsant <61548001+bagoesihsant@users.noreply.github.com>
2020-10-24 11:10:00 +0700

    Menambahkan datatables pada footer

--- application/views/templates/footer_js.php
+++ application/views/templates/footer_js.php
@@ -16,6 +16,11 @@
 </script>
 <!-- Bootstrap 4 -->
 <script src="<?= base_url('assets/'); ?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- DataTables -->
+<script src="<?= base_url('assets/'); ?>plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="<?= base_url('assets/'); ?>plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="<?= base_url('assets/'); ?>plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="<?= base_url('assets/'); ?>plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
 <!-- ChartJS -->
 <script src="<?= base_url('assets/'); ?>plugins/chart.js/Chart.min.js"></script>
 <!-- Sparkline -->
