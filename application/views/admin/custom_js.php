c4c5f2d61680db05294c0a20ce54ad73e05700fd
bagoesihsant <61548001+bagoesihsant@users.noreply.github.com>
2020-10-24 11:02:26 +0700

    Menambahkan Data Table tabel customer

---
+++ application/views/admin/custom_js.php
@@ -0,0 +1,9 @@
+<script>
+    $(function() {
+        // Mengubah tabel customer di customer menjadi data tables
+        $('#customerTable').DataTable({
+            "responsive": true,
+            "autoWidth": false,
+        });
+    });
+</script>
