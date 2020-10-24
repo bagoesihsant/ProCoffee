960924ff7ed04d81690232034b61bdaac0b6dbb9
afakihfaj <afakihfaj@gmail.com>
2020-10-25 04:22:46 +0700

    Menambah view lupa password admin side

---
+++ application/views/login_template/v_auth_footer.php
@@ -0,0 +1,10 @@
+<!-- jQuery -->
+<script src="<?= base_url('vendor'); ?>/AdminLTE/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="<?= base_url('vendor'); ?>/AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?= base_url('vendor'); ?>/dist/js/adminlte.min.js"></script>
+
+</body>
+
+</html>
