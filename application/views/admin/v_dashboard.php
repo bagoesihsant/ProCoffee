c7c797ea5c4e97ab6585896748fc0d53ca4c494f
bagoesihsant <61548001+bagoesihsant@users.noreply.github.com>
2020-10-24 11:09:50 +0700

    Debug view Dashboard

--- application/views/admin/v_dashboard.php
+++ application/views/admin/v_dashboard.php
@@ -149,7 +149,7 @@
                                         <span class="direct-chat-timestamp float-right">23 Jan 2:00 pm</span>
                                     </div>
                                     <!-- /.direct-chat-infos -->
-                                    <img class="direct-chat-img" src="dist/img/user1-128x128.jpg" alt="message user image">
+                                    <img class="direct-chat-img" src="<?= base_url('assets/'); ?>dist/img/user1-128x128.jpg" alt="message user image">
                                     <!-- /.direct-chat-img -->
                                     <div class="direct-chat-text">
                                         Is this template really for free? That's unbelievable!
@@ -165,7 +165,7 @@
                                         <span class="direct-chat-timestamp float-left">23 Jan 2:05 pm</span>
                                     </div>
                                     <!-- /.direct-chat-infos -->
-                                    <img class="direct-chat-img" src="dist/img/user3-128x128.jpg" alt="message user image">
+                                    <img class="direct-chat-img" src="<?= base_url('assets/'); ?>dist/img/user3-128x128.jpg" alt="message user image">
                                     <!-- /.direct-chat-img -->
                                     <div class="direct-chat-text">
                                         You better believe it!
@@ -181,7 +181,7 @@
                                         <span class="direct-chat-timestamp float-right">23 Jan 5:37 pm</span>
                                     </div>
                                     <!-- /.direct-chat-infos -->
-                                    <img class="direct-chat-img" src="dist/img/user1-128x128.jpg" alt="message user image">
+                                    <img class="direct-chat-img" src="<?= base_url('assets/'); ?>dist/img/user1-128x128.jpg" alt="message user image">
                                     <!-- /.direct-chat-img -->
                                     <div class="direct-chat-text">
                                         Working with AdminLTE on a great new app! Wanna join?
@@ -197,7 +197,7 @@
                                         <span class="direct-chat-timestamp float-left">23 Jan 6:10 pm</span>
                                     </div>
                                     <!-- /.direct-chat-infos -->
-                                    <img class="direct-chat-img" src="dist/img/user3-128x128.jpg" alt="message user image">
+                                    <img class="direct-chat-img" src="<?= base_url('assets/'); ?>dist/img/user3-128x128.jpg" alt="message user image">
                                     <!-- /.direct-chat-img -->
                                     <div class="direct-chat-text">
                                         I would love to.
@@ -214,7 +214,7 @@
                                 <ul class="contacts-list">
                                     <li>
                                         <a href="#">
-                                            <img class="contacts-list-img" src="dist/img/user1-128x128.jpg">
+                                            <img class="contacts-list-img" src="<?= base_url("assets/"); ?>dist/img/user1-128x128.jpg">
 
                                             <div class="contacts-list-info">
                                                 <span class="contacts-list-name">
@@ -229,7 +229,7 @@
                                     <!-- End Contact Item -->
                                     <li>
                                         <a href="#">
-                                            <img class="contacts-list-img" src="dist/img/user7-128x128.jpg">
+                                            <img class="contacts-list-img" src="<?= base_url("assets/"); ?>dist/img/user7-128x128.jpg">
 
                                             <div class="contacts-list-info">
                                                 <span class="contacts-list-name">
@@ -244,7 +244,7 @@
                                     <!-- End Contact Item -->
                                     <li>
                                         <a href="#">
-                                            <img class="contacts-list-img" src="dist/img/user3-128x128.jpg">
+                                            <img class="contacts-list-img" src="<?= base_url("assets/"); ?>dist/img/user3-128x128.jpg">
 
                                             <div class="contacts-list-info">
                                                 <span class="contacts-list-name">
@@ -259,7 +259,7 @@
                                     <!-- End Contact Item -->
                                     <li>
                                         <a href="#">
-                                            <img class="contacts-list-img" src="dist/img/user5-128x128.jpg">
+                                            <img class="contacts-list-img" src="<?= base_url("assets/"); ?>dist/img/user5-128x128.jpg">
 
                                             <div class="contacts-list-info">
                                                 <span class="contacts-list-name">
@@ -274,7 +274,7 @@
                                     <!-- End Contact Item -->
                                     <li>
                                         <a href="#">
-                                            <img class="contacts-list-img" src="dist/img/user6-128x128.jpg">
+                                            <img class="contacts-list-img" src="<?= base_url("assets/"); ?>dist/img/user6-128x128.jpg">
 
                                             <div class="contacts-list-info">
                                                 <span class="contacts-list-name">
@@ -289,7 +289,7 @@
                                     <!-- End Contact Item -->
                                     <li>
                                         <a href="#">
-                                            <img class="contacts-list-img" src="dist/img/user8-128x128.jpg">
+                                            <img class="contacts-list-img" src="<?= base_url("assets/"); ?>dist/img/user8-128x128.jpg">
 
                                             <div class="contacts-list-info">
                                                 <span class="contacts-list-name">
