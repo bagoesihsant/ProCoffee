78ac704b8297ca987335ffea5626d210340dbe1a
lulungsatrioprayuda <alungsatrio12@gmail.com>
2020-10-29 00:01:53 +0700

    Update v_units.php

--- application/views/admin/v_units.php
+++ application/views/admin/v_units.php
@@ -80,13 +80,13 @@
                         </tbody>
                         <!-- Tbody End -->
                         <!-- Tfoot -->
-                        <tfoot>
+                        <!-- <tfoot>
                             <tr>
                                 <th>No.</th>
                                 <th>Nama Units</th>
                                 <th class="text-center">Action</th>
                             </tr>
-                        </tfoot>
+                        </tfoot> -->
                         <!-- Tfoot End -->
                     </table>
                     <!-- Data Table End -->
