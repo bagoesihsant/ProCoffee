794f6794c268429cd4246433eb2a48dfb878d5c7
Irman <43021053+alfiannsx98@users.noreply.github.com>
2020-10-29 16:18:41 +0700

    biar gini dulu lah, penting bisa nampilin OAKWOAWKOAWKWAKO

--- GIS_ujicoba/application/controllers/C_user.php
+++ GIS_ujicoba/application/controllers/C_user.php
@@ -17,13 +17,50 @@ class C_user extends CI_Controller
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
 
         $data['dt_user'] = $this->db->get('user')->result_array();
+        
+        $this->form_validation->set_rules('nama', 'Nama', 'required');
+        $this->form_validation->set_rules('email', 'Email', 'required');
+        $this->form_validation->set_rules('image', 'Image', 'required');
+        $this->form_validation->set_rules('password', 'password', 'required');
+        $this->form_validation->set_rules('about', 'About', 'required');
+        $this->form_validation->set_rules('role_id', 'Role Id', 'required');
+        $this->form_validation->set_rules('date_created', 'Date Created', 'required');
+        $this->form_validation->set_rules('change_pass', 'Change Password', 'required');
 
-        $this->load->view('templates/header', $data);
-        $this->load->view('templates/sidebar', $data);
-        $this->load->view('templates/topbar', $data);
-        $this->load->view('management_user/index', $data);
-        $this->load->view('templates/custom-footer', $data);
-        $this->load->view('templates/dist-footer', $data);
-        $this->load->view('templates/footer', $data);
+        if($this->form_validation->run() == false){
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('management_user/index', $data);
+            $this->load->view('templates/custom-footer', $data);
+            $this->load->view('templates/dist-footer', $data);
+            $this->load->view('templates/footer', $data);
+        }else{
+            $q_count = $this->db->get('user')->num_rows();
+
+            if($q_count >= 10){
+                $id_user = "USR-0" . date('dm', time()).($q_count + 1);
+            }else if($q_count >= 100){
+                $id_user = "USR-" . date('dm', time()).($q_count + 1);
+            }else{
+                $id_user = "USR-00" . date('dm', time()).($q_count + 1);
+            }
+
+            $data = [
+                'id_user' => $id_user,
+                'nama' => $this->input->post('nama'),
+                'email' => $this->input->post('email'),
+                'image' => $this->input->post('image'),
+                'password' => time(),
+                'about' => $this->input->post('about'),
+                'role_id' => $this->input->post('role_id'),
+                'is_active' => 0,
+                'date_created' => time(),
+                'change_pass' => 0
+            ];
+            $this->db->insert('user', $data);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Data Berhasil Disimpan</div>');
+            redirect('menu');
+        }
     }
 }
--- GIS_ujicoba/application/views/management_user/index.php
+++ GIS_ujicoba/application/views/management_user/index.php
@@ -32,14 +32,12 @@
                     <thead>
                         <tr>
                             <th>#</th>
-                            <th>NIM</th>
                             <th>Nama</th>
                             <th>Email</th>
                             <th>Image</th>
-                            <th>About</th>
                             <th>Role</th>
                             <th>Status</th>
-                            <th>Tanggal Dibuat Akun</th>
+                            <th>Date Created</th>
                             <th>Actions</th>
                         </tr>
                     </thead>
@@ -50,17 +48,15 @@
                     ?>
                         <tr>
                             <td><?= $i; ?></td>
-                            <td><?= $m['identity']; ?></td>
                             <td><?= $m['nama']; ?></td>
                             <td><?= $m['email']; ?></td>
                             <td><?= $m['image']; ?></td>
-                            <td><?= $m['about']; ?></td>
                             <td><?= $m['role_id']; ?></td>
                             <td><?= $m['is_active']; ?></td>
                             <td><?= $m['date_created']; ?></td>
                             <td class="text-right">
                                 <button class="btn btn-info btn-xs btn-round" data-toggle="modal" data-target="#modal_edit<?= $id; ?>">Edit User</button>
-                                <button class="btn btn-secondary btn-xs btn-round" data-toggle="modal" data-target="#modal_hapus<?= $id; ?>">Hapus User</button>
+                                <button class="btn btn-danger btn-xs btn-round" data-toggle="modal" data-target="#modal_hapus<?= $id; ?>">Hapus User</button>
                             </td>
                         </tr>
                         <?php $i++; ?>
@@ -69,14 +65,12 @@
                     <tfoot>
                         <tr>
                             <th>#</th>
-                            <th>NIM</th>
                             <th>Nama</th>
                             <th>Email</th>
                             <th>Image</th>
-                            <th>About</th>
                             <th>Role</th>
                             <th>Status</th>
-                            <th>Tanggal Dibuat Akun</th>
+                            <th>Date Created</th>
                             <th>Actions</th>
                         </tr>
                     </tfoot>
@@ -105,8 +99,36 @@
                 <h5 class="modal-title" id="newroleModal">Create New Data</h5>
                 </button>   
             </div>
-            <form action="<?= base_url('menu'); ?>" method="post">
+            <form action="<?= base_url('C_user'); ?>" method="post">
                 <div class="modal-body">
+                    <div class="form-group">
+                        <label for="">menu</label>
+                        <input type="text" class="form-control" id="menu" name="menu" placeholder="Masukkan menu">
+                    </div>
+                    <div class="form-group">
+                        <label for="">menu</label>
+                        <input type="text" class="form-control" id="menu" name="menu" placeholder="Masukkan menu">
+                    </div>
+                    <div class="form-group">
+                        <label for="">menu</label>
+                        <input type="text" class="form-control" id="menu" name="menu" placeholder="Masukkan menu">
+                    </div>
+                    <div class="form-group">
+                        <label for="">menu</label>
+                        <input type="text" class="form-control" id="menu" name="menu" placeholder="Masukkan menu">
+                    </div>
+                    <div class="form-group">
+                        <label for="">menu</label>
+                        <input type="text" class="form-control" id="menu" name="menu" placeholder="Masukkan menu">
+                    </div>
+                    <div class="form-group">
+                        <label for="">menu</label>
+                        <input type="text" class="form-control" id="menu" name="menu" placeholder="Masukkan menu">
+                    </div>
+                    <div class="form-group">
+                        <label for="">menu</label>
+                        <input type="text" class="form-control" id="menu" name="menu" placeholder="Masukkan menu">
+                    </div>
                     <div class="form-group">
                         <label for="">menu</label>
                         <input type="text" class="form-control" id="menu" name="menu" placeholder="Masukkan menu">
@@ -124,70 +146,3 @@
         </div>
     </div>
 </div>
-
-<!--MODAL EDIT role!-->
-<?php
-foreach ($menu as $i) :
-    $id = $i['id_menu'];
-    $menu = $i['menu'];
-    $icon_menu = $i['icon'];
-    ?>
-
-<div class="modal fade" id="modal_edit<?= $id; ?>" tabindex="-1" role="dialog" aria-labelledby="largeModal" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h3 class="modal-title" id="myModalLabel">Edit role</h3>
-            </div>
-            <form action="<?= base_url() . 'menu/edit_menu'; ?>" method="post" class="form-horizontal">
-                <div class="modal-body">
-                    <div class="form-group" hidden>
-                        <label class="control-label col-xs-3">ID menu</label>
-                        <div class="col-xs-8">
-                            <input name="id_menu" value="<?php echo $id; ?>" class="form-control" type="text" placeholder="ID menu" hidden>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label col-xs-3">Nama menu</label>
-                        <div class="col-xs-8">
-                            <input name="menu" value="<?php echo $menu; ?>" class="form-control" type="text" placeholder="Nama Menu" required>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label col-xs-3">Icon Menu</label>
-                        <div class="col-xs-8">
-                            <input name="icon" value="<?php echo $icon_menu; ?>" class="form-control" type="text" placeholder="Icon Menu" required>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button class="btn" data-dismiss="modal" aria-hidden="true">Tutup</button>
-                        <button class="btn btn-info">Update</button>
-                    </div>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
-
-<!--MODAL HAPUS DATA!-->
-<div class="modal fade" id="modal_hapus<?= $id; ?>" tabindex="-1" role="dialog" aria-labelledby="largeModal" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h3 class="modal-title" id="myModalLabel">Hapus Menu</h3>
-            </div>
-            <form action="<?= base_url() . 'menu/hapus_menu'; ?>" method="post" class="form-horizontal">
-                <div class="modal-body">
-                    <p>Apakah Anda yakin mau menghapus data ini? <b><?= $menu; ?></b></p>
-                </div>
-                <div class="modal-footer">
-                    <input type="hidden" name="id_menu" value="<?= $id; ?>">
-                    <button class="btn" data-dismiss="modal" aria-hidden="true">Tutup</button>
-                    <button class="btn btn-danger">Hapus</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
-
-<?php endforeach; ?>
