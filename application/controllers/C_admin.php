35a940f868693815addfc4ce24427d2d3630f0c4
bagoesihsant <61548001+bagoesihsant@users.noreply.github.com>
2020-10-31 19:35:04 +0700

    Melengkapi fungsi index_menu

--- application/controllers/C_admin.php
+++ application/controllers/C_admin.php
@@ -6,6 +6,15 @@ class C_admin extends CI_Controller
 {
 
 
+    // Construct
+    public function __construct()
+    {
+        parent::__construct();
+
+        // Load Model Menu
+        $this->load->model('M_menu', 'menu');
+    }
+
     // Index
     public function index()
     {
@@ -75,11 +84,64 @@ class C_admin extends CI_Controller
     // Menu
     public function index_menu()
     {
-        $this->load->view('templates/v_header_admin');
-        $this->load->view('templates/v_sidebar_admin');
-        $this->load->view('admin/v_menu');
-        $this->load->view('templates/footer_js');
-        $this->load->view('admin/custom_js');
-        $this->load->view('templates/v_footer_admin');
+
+        // Membuat variabel array data
+        // Mengambil isi menu dari database
+        $data['menu'] = $this->menu->getAllMenu();
+
+        // Membuat rule untuk validasi form
+        $this->form_validation->set_rules('kode_menu', 'Kode Menu', 'required|trim|alpha_numeric');
+        $this->form_validation->set_rules('menu', 'Menu', 'required|trim|alpha_numeric_spaces');
+
+        // Membuat pesan kustom untuk rule validasi form
+        $this->form_validation->set_message('required', 'Field %s wajib diisi.');
+        $this->form_validation->set_message('alpha_numeric_spaces', 'Field %s hanya boleh berisikan angka dan huruf.');
+
+        // Melakukan validasi form
+        if ($this->form_validation->run() == false) {
+            // Jika hasil validasi form mengembalikan false
+            $this->load->view('templates/v_header_admin', $data);
+            $this->load->view('templates/v_sidebar_admin');
+            $this->load->view('admin/v_menu');
+            $this->load->view('templates/footer_js');
+            $this->load->view('admin/custom_js');
+            $this->load->view('templates/v_footer_admin');
+        } else {
+            // Jika hasil validasi form mengembalikan true
+
+            // Membuat array data
+            $data = [
+                'kode_menu' => htmlspecialchars($this->input->post('kode_menu', true)),
+                'menu' => htmlspecialchars(ucwords($this->input->post('menu', true)))
+            ];
+
+            // Melakukan penambahan data
+            $result = $this->menu->tambahMenu($data);
+
+            // Memeriksa apakah proses insert berhasil atau tidak
+            if ($result > 0) {
+                // Jika proses insert berhasil
+
+                // Membuat Session
+                $this->session->set_flashdata(
+                    'pesan_menu',
+                    'toastr.success("Selamat, Data berhasil ditambahkan.")'
+                );
+
+                // Mengarahkan ulang
+                redirect('admin/menu');
+            } else {
+                // Jika proses insert tidak berhasil
+
+                // Membuat Session
+                $this->session->set_flashdata(
+                    'pesan_menu',
+                    'toastr.error("Error, Data gagal ditambahkan.")'
+                );
+
+                // Mengarahkan ulang
+                redirect('admin/menu');
+            }
+        }
     }
 }
