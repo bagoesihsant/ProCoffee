21d7b9ac5e4990c020c9e97898a3cb819dd58cc5
bagoesihsant <61548001+bagoesihsant@users.noreply.github.com>
2020-10-31 23:53:17 +0700

    Menambahkan fungsi
    Menambahkan fungsi untuk hapus data.
    
    Menambahkan fungsi untuk menampilkan data.
    
    Menambahkan fungsi untuk mengubah data.

--- application/views/admin/custom_js.php
+++ application/views/admin/custom_js.php
@@ -2,57 +2,149 @@
     // Mencetak pesan dari menu
     <?= $this->session->flashdata('pesan_menu'); ?>
 
+    // Mengubah tabel customer di customer menjadi data tables
+    $('#customerTable').DataTable({
+        "responsive": true,
+        "autoWidth": false,
+    });
 
-    $(function() {
-        // Mengubah tabel customer di customer menjadi data tables
-        $('#customerTable').DataTable({
-            "responsive": true,
-            "autoWidth": false,
-        });
-
-        // Mengubah tabel menu di menu menjadi data tables
-        $('#dataTableMenu').DataTable({
-            "responsive": true,
-            "autoWidth": false,
-        });
+    // Mengubah tabel menu di menu menjadi data tables
+    $('#dataTableMenu').DataTable({
+        "responsive": true,
+        "autoWidth": false,
+    });
 
 
-        // Menggunakan sweet alert untuk menghapus data
-        // Kustomisasi styling tombol untuk sweet alert menggunakan bootstrap
-        const swalBootstrap = Swal.mixin({
-            customClass: {
-                confirmButton: 'btn btn-success mx-2 px-4',
-                cancelButton: 'btn btn-danger mx-2'
+    // Menjalankan Ajax untuk mengambil data dari database
+    $('#dataTableMenu tbody').on('click', '.btn-edit-menu', function() {
+
+        // Mengambil data melalui attribute data pada elemen html
+        const kodeMenu = $(this).data('kode');
+
+        // Mengambil elemen form yang akan diisi dengan data ajax
+        const formMenu = $('#formEditMenu');
+
+        // Melakukan Ajax
+        $.ajax({
+            url: "http://localhost/ProCoffee/C_admin/ajaxDataEditMenu",
+            data: {
+                kode_menu: kodeMenu
             },
-            buttonsStyling: false
+            method: "post",
+            dataType: "json",
+            success: function(data) {
+                // Jika ajax berhasil dijalankan
+                formMenu.find('#kode_menu').val(data.kode_menu);
+                formMenu.find('#menu').val(data.menu);
+            },
+            error: function(e) {
+                // Jika ajax gagal dijalankan
+                console.log(e);
+            }
         });
 
-        // Jika tombol delete di tabel customer di klik, maka muncul sweet alert
-        $('.btnDeleteCustomer').on('click', function() {
-            swalBootstrap.fire({
-                title: "Apakah anda yakin ?",
-                text: "Apakah anda yakin akan menghapus data ?",
-                icon: "warning",
-                showCancelButton: true,
-                confirmButtonText: "Ya",
-                cancelButtonText: "Tidak",
-                reverseButtons: true
-            }).then((hasil) => {
-                if (hasil.isConfirmed) {
-                    swalBootstrap.fire({
-                        title: "Terhapus",
-                        text: "Data berhasil dihapus",
-                        icon: "success"
-                    })
-                } else if (hasil.dismiss == Swal.DismissReason.cancel) {
-                    swalBootstrap.fire({
-                        title: "Batal",
-                        text: "Data batal dihapus",
-                        icon: "error"
-                    })
-                }
-            })
+    });
+
+    // Menjalankan ajax untuk mengambil data dari database
+    $('#dataTableMenu tbody').on('click', '.btn-view-menu', function() {
+
+        // Mengambil data melalui attribute data pada elemen html
+        const kodeMenu = $(this).data('kode');
+
+        // Mengambil elemen form yang akan diisi dengan data ajax
+        const modalMenu = $('#viewModal');
+
+        // Melakukan Ajax
+        $.ajax({
+            url: "http://localhost/ProCoffee/C_admin/ajaxDataEditMenu",
+            data: {
+                kode_menu: kodeMenu
+            },
+            method: "post",
+            dataType: "json",
+            success: function(data) {
+                // Jika ajax berhasil dijalankan
+                modalMenu.find('#kode_menu').val(data.kode_menu);
+                modalMenu.find('#menu').val(data.menu);
+
+            },
+            error: function(e) {
+                // Jika ajax gagal dijalankan
+                console.log(e);
+            }
         });
 
+    });
+
+    // Menjalankan fungsi hapus pada tabel
+    $('#dataTableMenu tbody').on('click', '.btn-delete-menu', function() {
+
+        // Mengambil data melalui attribute data pada elemen html
+        const kodeMenu = $(this).data('kode');
+
+        // Membuka Sweet Alert
+        Swal.fire({
+            title: "Hapus Data",
+            text: "Apakah anda yakin akan menghapus data ini ?",
+            icon: "error",
+            buttonsStyling: false,
+            showCancelButton: true,
+            showConfirmButton: true,
+            confirmButtonText: "Ya",
+            cancelButtonText: "Tidak",
+            customClass: {
+                confirmButton: "btn btn-primary px-4 mx-2",
+                cancelButton: "btn btn-danger px-4 mx-2"
+            }
+        }).then((result) => {
+            if (result.value) {
+                // Memindahkan ke halaman lain jika di konfirmasi
+                window.location.href = "http://localhost/ProCoffee/admin/hapusMenu/" + kodeMenu;
+            } else if (result.dismiss == Swal.DismissReason.cancel) {
+                // Menutup sweet alert jika di cancel
+                Swal.close();
+            }
+        });
+
+    });
+
+
+
+
+    // Menggunakan sweet alert untuk menghapus data
+    // Kustomisasi styling tombol untuk sweet alert menggunakan bootstrap
+    const swalBootstrap = Swal.mixin({
+        customClass: {
+            confirmButton: 'btn btn-success mx-2 px-4',
+            cancelButton: 'btn btn-danger mx-2'
+        },
+        buttonsStyling: false
+    });
+
+    // Jika tombol delete di tabel customer di klik, maka muncul sweet alert
+    $('.btnDeleteCustomer').on('click', function() {
+        swalBootstrap.fire({
+            title: "Apakah anda yakin ?",
+            text: "Apakah anda yakin akan menghapus data ?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonText: "Ya",
+            cancelButtonText: "Tidak",
+            reverseButtons: true
+        }).then((hasil) => {
+            if (hasil.isConfirmed) {
+                swalBootstrap.fire({
+                    title: "Terhapus",
+                    text: "Data berhasil dihapus",
+                    icon: "success"
+                })
+            } else if (hasil.dismiss == Swal.DismissReason.cancel) {
+                swalBootstrap.fire({
+                    title: "Batal",
+                    text: "Data batal dihapus",
+                    icon: "error"
+                })
+            }
+        })
     });
 </script>
