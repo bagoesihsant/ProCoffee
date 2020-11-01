ddb5d7476bdbb67aa3af1fdf17a295623ccf063c
E41181233_Yosef Triadi Setiawan Santoso <55664947+YosefTriadi17@users.noreply.github.com>
2020-11-01 13:29:31 +0700

    Update v_sidebar_admin.php

--- application/views/templates/v_sidebar_admin.php
+++ application/views/templates/v_sidebar_admin.php
@@ -40,7 +40,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="index_supplier" class="nav-link">
+                    <a href="<?= base_url()."C_admin/index_supplier" ?>" class="nav-link">
                         <i class="nav-icon fas fa-fw fa-truck"></i>
                         <p>Supplier</p>
                     </a>
@@ -52,6 +52,7 @@
                             Product
                             <i class="right fas fa-angle-left"></i>
                         </p>
+<<<<<<< Updated upstream
                     </a>
 
                     <ul class="nav nav-treeview">
@@ -69,6 +70,25 @@
                         </li>
                         <li class="nav-item">
                             <a href="index_product_items" class="nav-link">
+=======
+                        </a>
+                        
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="<?= base_url()."C_admin/index_product_categories" ?>" class="nav-link">
+                                <i class="nav-icon far fa-circle text-secondary"></i>
+                                <p>Categories</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="<?= base_url()."C_admin/index_product_units" ?>" class="nav-link">
+                                <i class="nav-icon far fa-circle text-secondary"></i>
+                                <p>Units</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="<?= base_url()."C_admin/index_product_items" ?>" class="nav-link">
+>>>>>>> Stashed changes
                                 <i class="nav-icon far fa-circle text-secondary"></i>
                                 <p>Items</p>
                             </a>
