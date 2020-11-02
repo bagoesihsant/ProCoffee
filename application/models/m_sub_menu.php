93e80a9f3bec514279e9f706ab2de3e06310b265
bagoesihsant <61548001+bagoesihsant@users.noreply.github.com>
2020-11-02 13:12:17 +0700

    Pembuatan fungsi tambahan pada model sub menu

--- application/models/m_sub_menu.php
+++ application/models/m_sub_menu.php
@@ -6,7 +6,10 @@ class M_sub_menu extends CI_Model
     // Fungsi untuk mengambil semua data sub menu pada tabel
     public function getAllSubMenu()
     {
-        return $this->db->get('user_sub_menu')->result_array();
+        $this->db->select('*');
+        $this->db->from('user_sub_menu');
+        $this->db->join('user_menu', 'user_sub_menu.kode_menu = user_menu.kode_menu');
+        return $this->db->get()->result_array();
     }
 
     // Fungsi untuk mengambil data terakhir pada tabel
@@ -14,5 +17,12 @@ class M_sub_menu extends CI_Model
     {
         $this->db->order_by('kode_sub_menu', 'DESC');
         return $this->db->get('user_sub_menu');
+    }
+
+    // Fungsi untuk menambahkan data sub menu pada tabel
+    public function tambahSubmenu($data)
+    {
+        $this->db->insert('user_sub_menu', $data);
+        return $this->db->affected_rows();
     }
 }
