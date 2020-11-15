7333b93f23d6d3d362845b9df09577633e1c8dff
bagoesihsant <61548001+bagoesihsant@users.noreply.github.com>
2020-11-15 08:46:19 +0700

    Pembuatan default controller untuk masing masing subolder

---
+++ application/controllers/Kasir/C_kasir.php
@@ -0,0 +1,18 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class C_kasir extends CI_Controller
+{
+
+    // Construct
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+    public function index()
+    {
+    }
+}
