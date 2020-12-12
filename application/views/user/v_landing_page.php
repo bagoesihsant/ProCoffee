0395e5c76335cfd8862781fb4dfdb1e439fd1b18
lulungsatrioprayuda <alungsatrio12@gmail.com>
2020-12-12 16:53:15 +0700

    Update v_landing_page.php

--- application/views/user/v_landing_page.php
+++ application/views/user/v_landing_page.php
@@ -1,10 +1,7 @@
 <body>
     <!-- navbar-->
     <header class="header mb-5">
-        <!--
-      *** TOPBAR ***
-      _________________________________________________________
-      -->
+        <!-------- *** TOPBAR ***_________________________________________________________-->
         <div id="top">
             <div class="container">
                 <div class="row">
@@ -38,8 +35,8 @@
                                     <button class="btn btn-primary"><i class="fa fa-sign-in"></i> Log in</button>
                                 </p>
                             </form>
-                            <p class="text-center text-muted">Not registered yet?</p>
-                            <p class="text-center text-muted"><a href="register.html"><strong>Register now</strong></a>! It is easy and done in 1 minute and gives you access to special discounts and much more!</p>
+                            <p class="text-center text-muted">Belum Punya Akun?</p>
+                            <p class="text-center text-muted"><a href="register.html"><strong>Register Sekarang</strong></a>! Mudah dan hanya butuh beberapa saat untuk mendapatkan akses beli barang di website kami</p>
                         </div>
                     </div>
                 </div>
@@ -57,20 +54,24 @@
                 <div id="navigation" class="collapse navbar-collapse">
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item"><a href="#" class="nav-link active">Home</a></li>
-                        <li class="nav-item dropdown menu-large"><a href="#" data-toggle="dropdown" data-hover="dropdown" data-delay="200" class="dropdown-toggle nav-link">Men<b class="caret"></b></a>
+                        <li class="nav-item dropdown menu-large"><a href="#" data-toggle="dropdown" data-hover="dropdown" data-delay="200" class="dropdown-toggle nav-link">User<b class="caret"></b></a>
                             <ul class="dropdown-menu megamenu">
                                 <li>
                                     <div class="row">
                                         <div class="col-md-6 col-lg-3">
-                                            <h5>Clothing</h5>
+                                            <h5>Transaksi</h5>
                                             <ul class="list-unstyled mb-3">
-                                                <li class="nav-item"><a href="category.html" class="nav-link">T-shirts</a></li>
-                                                <li class="nav-item"><a href="category.html" class="nav-link">Shirts</a></li>
-                                                <li class="nav-item"><a href="category.html" class="nav-link">Pants</a></li>
-                                                <li class="nav-item"><a href="category.html" class="nav-link">Accessories</a></li>
+                                                <li class="nav-item"><a href="category.html" class="nav-link">Keranjang</a></li>
+                                                <li class="nav-item"><a href="category.html" class="nav-link">Riwayat Pembelian</a></li>
                                             </ul>
                                         </div>
                                         <div class="col-md-6 col-lg-3">
+                                            <h5>Lainnya</h5>
+                                            <ul class="list-unstyled mb-3">
+                                                <li class="nav-item"><a href="category.html" class="nav-link">LogOut</a></li>
+                                            </ul>
+                                        </div>
+                                        <!-- <div class="col-md-6 col-lg-3">
                                             <h5>Shoes</h5>
                                             <ul class="list-unstyled mb-3">
                                                 <li class="nav-item"><a href="category.html" class="nav-link">Trainers</a></li>
@@ -78,37 +79,12 @@
                                                 <li class="nav-item"><a href="category.html" class="nav-link">Hiking shoes</a></li>
                                                 <li class="nav-item"><a href="category.html" class="nav-link">Casual</a></li>
                                             </ul>
-                                        </div>
-                                        <div class="col-md-6 col-lg-3">
-                                            <h5>Accessories</h5>
-                                            <ul class="list-unstyled mb-3">
-                                                <li class="nav-item"><a href="category.html" class="nav-link">Trainers</a></li>
-                                                <li class="nav-item"><a href="category.html" class="nav-link">Sandals</a></li>
-                                                <li class="nav-item"><a href="category.html" class="nav-link">Hiking shoes</a></li>
-                                                <li class="nav-item"><a href="category.html" class="nav-link">Casual</a></li>
-                                                <li class="nav-item"><a href="category.html" class="nav-link">Hiking shoes</a></li>
-                                                <li class="nav-item"><a href="category.html" class="nav-link">Casual</a></li>
-                                            </ul>
-                                        </div>
-                                        <div class="col-md-6 col-lg-3">
-                                            <h5>Featured</h5>
-                                            <ul class="list-unstyled mb-3">
-                                                <li class="nav-item"><a href="category.html" class="nav-link">Trainers</a></li>
-                                                <li class="nav-item"><a href="category.html" class="nav-link">Sandals</a></li>
-                                                <li class="nav-item"><a href="category.html" class="nav-link">Hiking shoes</a></li>
-                                            </ul>
-                                            <h5>Looks and trends</h5>
-                                            <ul class="list-unstyled mb-3">
-                                                <li class="nav-item"><a href="category.html" class="nav-link">Trainers</a></li>
-                                                <li class="nav-item"><a href="category.html" class="nav-link">Sandals</a></li>
-                                                <li class="nav-item"><a href="category.html" class="nav-link">Hiking shoes</a></li>
-                                            </ul>
-                                        </div>
+                                        </div> -->
                                     </div>
                                 </li>
                             </ul>
                         </li>
-                        <li class="nav-item dropdown menu-large"><a href="#" data-toggle="dropdown" data-hover="dropdown" data-delay="200" class="dropdown-toggle nav-link">Ladies<b class="caret"></b></a>
+                        <!-- <li class="nav-item dropdown menu-large"><a href="#" data-toggle="dropdown" data-hover="dropdown" data-delay="200" class="dropdown-toggle nav-link">Ladies<b class="caret"></b></a>
                             <ul class="dropdown-menu megamenu">
                                 <li>
                                     <div class="row">
@@ -154,8 +130,8 @@
                                     </div>
                                 </li>
                             </ul>
-                        </li>
-                        <li class="nav-item dropdown menu-large"><a href="#" data-toggle="dropdown" data-hover="dropdown" data-delay="200" class="dropdown-toggle nav-link">Template<b class="caret"></b></a>
+                        </li> -->
+                        <!-- <li class="nav-item dropdown menu-large"><a href="#" data-toggle="dropdown" data-hover="dropdown" data-delay="200" class="dropdown-toggle nav-link">Template<b class="caret"></b></a>
                             <ul class="dropdown-menu megamenu">
                                 <li>
                                     <div class="row">
@@ -204,11 +180,11 @@
                                     </div>
                                 </li>
                             </ul>
-                        </li>
+                        </li> -->
                     </ul>
                     <div class="navbar-buttons d-flex justify-content-end">
                         <!-- /.nav-collapse-->
-                        <div id="search-not-mobile" class="navbar-collapse collapse"></div><a data-toggle="collapse" href="#search" class="btn navbar-btn btn-primary d-none d-lg-inline-block"><span class="sr-only">Toggle search</span><i class="fa fa-search"></i></a>
+                        <!-- <div id="search-not-mobile" class="navbar-collapse collapse"></div><a data-toggle="collapse" href="#search" class="btn navbar-btn btn-primary d-none d-lg-inline-block"><span class="sr-only">Toggle search</span><i class="fa fa-search"></i></a> -->
                         <div id="basket-overview" class="navbar-collapse collapse d-none d-lg-block"><a href="basket.html" class="btn btn-primary navbar-btn"><i class="fa fa-shopping-cart"></i><span>3 items in cart</span></a></div>
                     </div>
                 </div>
@@ -286,7 +262,7 @@
                     <div class="container">
                         <div class="row">
                             <div class="col-md-12">
-                                <h2 class="mb-0">Hot this week</h2>
+                                <h2 class="mb-0">PRODUK KAMI</h2>
                             </div>
                         </div>
                     </div>
