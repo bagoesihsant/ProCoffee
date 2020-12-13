9a2b3f66b9ce91cb6be354f89a70634531bd132c
lulungsatrioprayuda <alungsatrio12@gmail.com>
2020-12-13 23:30:33 +0700

    Update v_landing_page.php

--- application/views/user/v_landing_page.php
+++ application/views/user/v_landing_page.php
@@ -295,8 +295,8 @@
         <div class="box text-center">
             <div class="container">
                 <div class="col-md-12">
-                    <h3 class="text-uppercase">From our blog</h3>
-                    <p class="lead mb-0">What's new in the world of fashion? <a href="blog.html">Check our blog!</a></p>
+                    <h3 class="text-uppercase">Ayo beli sekarang juga</h3>
+                    <!-- <p class="lead mb-0">What's new in the world of fashion? <a href="blog.html">Check our blog!</a></p> -->
                 </div>
             </div>
         </div>
@@ -309,7 +309,7 @@
                             <p class="author-category">By <a href="#">John Slim</a> in <a href="">Fashion and style</a></p>
                             <hr>
                             <p class="intro">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-                            <p class="read-more"><a href="post.html" class="btn btn-primary">Continue reading</a></p>
+                            <!-- <p class="read-more"><a href="post.html" class="btn btn-primary">Continue reading</a></p> -->
                         </div>
                     </div>
                     <div class="col-sm-6">
@@ -318,7 +318,7 @@
                             <p class="author-category">By <a href="#">John Slim</a> in <a href="">About Minimal</a></p>
                             <hr>
                             <p class="intro">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-                            <p class="read-more"><a href="post.html" class="btn btn-primary">Continue reading</a></p>
+                            <!-- <p class="read-more"><a href="post.html" class="btn btn-primary">Continue reading</a></p> -->
                         </div>
                     </div>
                 </div>
