5c590e34f45f935f7c40fb91786962816b2e07e0
lulungsatrioprayuda <alungsatrio12@gmail.com>
2020-12-13 22:48:30 +0700

    ui footer

--- application/views/templates/user_template/v_footer_user.php
+++ application/views/templates/user_template/v_footer_user.php
@@ -5,7 +5,7 @@
     <div id="footer">
         <div class="container">
             <div class="row">
-                <div class="col-lg-4 col-md-6">
+                <div class="col-lg-3 col-md-6">
                     <h4 class="mb-3">Pages</h4>
                     <ul class="list-unstyled">
                         <li><a href="text.html">About us</a></li>
@@ -20,8 +20,8 @@
                         <li><a href="register.html">Regiter</a></li>
                     </ul>
                 </div>
-                <!-- /.col-lg-4-->
-                <div class="col-lg-4 col-md-6">
+                <!-- /.col-lg-3-->
+                <div class="col-lg-3 col-md-6">
                     <h4 class="mb-3">Top categories</h4>
                     <h5>Men</h5>
                     <ul class="list-unstyled">
@@ -37,11 +37,15 @@
                         <li><a href="category.html">Accessories</a></li>
                     </ul>
                 </div>
-                <!-- /.col-lg-4-->
-                <div class="col-lg-4 col-md-6">
+                <!-- /.col-lg-3-->
+                <div class="col-lg-3 col-md-6">
                     <h4 class="mb-3">Where to find us</h4>
                     <p><strong>Obaju Ltd.</strong><br>13/25 New Avenue<br>New Heaven<br>45Y 73J<br>England<br><strong>Great Britain</strong></p><a href="contact.html">Go to contact page</a>
                     <hr class="d-block d-md-none">
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="mb-3">Sosial Media Kami</h4>
+                    <p class="social"><a href="#" class="facebook external"><i class="fa fa-facebook"></i></a><a href="#" class="twitter external"><i class="fa fa-twitter"></i></a><a href="#" class="instagram external"><i class="fa fa-instagram"></i></a><a href="#" class="gplus external"><i class="fa fa-google-plus"></i></a><a href="#" class="email external"><i class="fa fa-envelope"></i></a></p>
                 </div>
             </div>
             <!-- /.row-->
