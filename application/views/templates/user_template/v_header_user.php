a159b5e4905518c1e42159aa450dabd1a1a4be76
lulungsatrioprayuda <alungsatrio12@gmail.com>
2020-12-13 23:02:32 +0700

    Update v_header_user.php

--- application/views/templates/user_template/v_header_user.php
+++ application/views/templates/user_template/v_header_user.php
@@ -36,13 +36,11 @@
         <div id="top">
             <div class="container">
                 <div class="row">
-                    <div class="col-lg-6 offer mb-3 mb-lg-0"><a href="#" class="btn btn-success btn-sm">Offer of the day</a><a href="#" class="ml-1">Get flat 35% off on orders over $50!</a></div>
+                    <div class="col-lg-6 offer mb-3 mb-lg-0"></div>
                     <div class="col-lg-6 text-center text-lg-right">
                         <ul class="menu list-inline mb-0">
                             <li class="list-inline-item"><a href="#" data-toggle="modal" data-target="#login-modal">Login</a></li>
                             <li class="list-inline-item"><a href="register.html">Register</a></li>
-                            <li class="list-inline-item"><a href="contact.html">Contact</a></li>
-                            <li class="list-inline-item"><a href="#">Recently viewed</a></li>
                         </ul>
                     </div>
                 </div>
@@ -51,7 +49,7 @@
                 <div class="modal-dialog modal-sm">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title">Customer login</h5>
+                            <h5 class="modal-title">Login</h5>
                             <button type="button" data-dismiss="modal" aria-label="Close" class="close"><span aria-hidden="true">×</span></button>
                         </div>
                         <div class="modal-body">
