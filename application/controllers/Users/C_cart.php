2ff685558deee25cf53d4cb03918f37031480b01
lulungsatrioprayuda <alungsatrio12@gmail.com>
2020-12-21 15:36:45 +0700

    Title

--- application/controllers/Users/C_cart.php
+++ application/controllers/Users/C_cart.php
@@ -11,7 +11,8 @@ class C_cart extends CI_Controller
 
     public function index()
     {
-        $this->load->view('templates/user_template/v_header_user');
+        $data['title'] = "Keranjang";
+        $this->load->view('templates/user_template/v_header_user', $data);
         $this->load->view('User/v_cart_user');
         $this->load->view('templates/user_template/v_footer_user');
     }
