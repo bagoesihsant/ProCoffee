357bf9ae6f456f618a4523dfbc43066cbf62ded8
lulungsatrioprayuda <alungsatrio12@gmail.com>
2020-12-21 20:59:25 +0700

    Update v_landing_page.php

--- application/views/user/v_landing_page.php
+++ application/views/user/v_landing_page.php
@@ -67,30 +67,16 @@
                             <div class="flip-container">
                                 <div class="flipper">
                                     <div class="front"><a href="detail.html"><img src="<?= base_url('assets/vendor_user/'); ?>img/product1.jpg" alt="" class="img-fluid"></a></div>
-                                    <div class="back"><a href="detail.html"><img src="<?= base_url('assets/vendor_user/'); ?>img/product1_2.jpg" alt="" class="img-fluid"></a></div>
+
                                 </div>
                             </div><a href="detail.html" class="invisible"><img src="<?= base_url('assets/vendor_user/'); ?>img/product1.jpg" alt="" class="img-fluid"></a>
                             <div class="text">
-                                <h3><a href="detail.html">Fur coat with very but very very long name</a></h3>
+                                <h3><a href="detail.html">Kopi Rempah</a></h3>
                                 <p class="price">
-                                    <del></del>$143.00
+                                    <del></del>Rp 45.000
                                 </p>
                             </div>
                             <!-- /.text-->
-                            <div class="ribbon sale">
-                                <div class="theribbon">SALE</div>
-                                <div class="ribbon-background"></div>
-                            </div>
-                            <!-- /.ribbon-->
-                            <div class="ribbon new">
-                                <div class="theribbon">NEW</div>
-                                <div class="ribbon-background"></div>
-                            </div>
-                            <!-- /.ribbon-->
-                            <div class="ribbon gift">
-                                <div class="theribbon">GIFT</div>
-                                <div class="ribbon-background"></div>
-                            </div>
                             <!-- /.ribbon-->
                         </div>
                         <!-- /.product-->
@@ -100,30 +86,17 @@
                             <div class="flip-container">
                                 <div class="flipper">
                                     <div class="front"><a href="detail.html"><img src="<?= base_url('assets/vendor_user/'); ?>img/product2.jpg" alt="" class="img-fluid"></a></div>
-                                    <div class="back"><a href="detail.html"><img src="<?= base_url('assets/vendor_user/'); ?>img/product2_2.jpg" alt="" class="img-fluid"></a></div>
+
                                 </div>
                             </div><a href="detail.html" class="invisible"><img src="<?= base_url('assets/vendor_user/'); ?>img/product2.jpg" alt="" class="img-fluid"></a>
                             <div class="text">
-                                <h3><a href="detail.html">White Blouse Armani</a></h3>
+                                <h3><a href="detail.html">Kopi Original</a></h3>
                                 <p class="price">
-                                    <del>$280</del>$143.00
+                                    <del></del>Rp 45.000
                                 </p>
                             </div>
                             <!-- /.text-->
-                            <div class="ribbon sale">
-                                <div class="theribbon">SALE</div>
-                                <div class="ribbon-background"></div>
-                            </div>
-                            <!-- /.ribbon-->
-                            <div class="ribbon new">
-                                <div class="theribbon">NEW</div>
-                                <div class="ribbon-background"></div>
-                            </div>
-                            <!-- /.ribbon-->
-                            <div class="ribbon gift">
-                                <div class="theribbon">GIFT</div>
-                                <div class="ribbon-background"></div>
-                            </div>
+
                             <!-- /.ribbon-->
                         </div>
                         <!-- /.product-->
@@ -133,13 +106,13 @@
                             <div class="flip-container">
                                 <div class="flipper">
                                     <div class="front"><a href="detail.html"><img src="<?= base_url('assets/vendor_user/'); ?>img/product3.jpg" alt="" class="img-fluid"></a></div>
-                                    <div class="back"><a href="detail.html"><img src="<?= base_url('assets/vendor_user/'); ?>img/product3_2.jpg" alt="" class="img-fluid"></a></div>
+
                                 </div>
                             </div><a href="detail.html" class="invisible"><img src="<?= base_url('assets/vendor_user/'); ?>img/product3.jpg" alt="" class="img-fluid"></a>
                             <div class="text">
-                                <h3><a href="detail.html">Black Blouse Versace</a></h3>
+                                <h3><a href="detail.html">Kopi Gingseng</a></h3>
                                 <p class="price">
-                                    <del></del>$143.00
+                                    <del></del>Rp. 50.000
                                 </p>
                             </div>
                             <!-- /.text-->
@@ -151,13 +124,13 @@
                             <div class="flip-container">
                                 <div class="flipper">
                                     <div class="front"><a href="detail.html"><img src="<?= base_url('assets/vendor_user/'); ?>img/product3.jpg" alt="" class="img-fluid"></a></div>
-                                    <div class="back"><a href="detail.html"><img src="<?= base_url('assets/vendor_user/'); ?>img/product3_2.jpg" alt="" class="img-fluid"></a></div>
+
                                 </div>
                             </div><a href="detail.html" class="invisible"><img src="<?= base_url('assets/vendor_user/'); ?>img/product3.jpg" alt="" class="img-fluid"></a>
                             <div class="text">
-                                <h3><a href="detail.html">Black Blouse Versace</a></h3>
+                                <h3><a href="detail.html">Kopi Stamina</a></h3>
                                 <p class="price">
-                                    <del></del>$143.00
+                                    <del></del>Rp. 55.000
                                 </p>
                             </div>
                             <!-- /.text-->
@@ -173,9 +146,9 @@
                                 </div>
                             </div><a href="detail.html" class="invisible"><img src="<?= base_url('assets/vendor_user/'); ?>img/product2.jpg" alt="" class="img-fluid"></a>
                             <div class="text">
-                                <h3><a href="detail.html">White Blouse Versace</a></h3>
+                                <h3><a href="detail.html">Kopi Lanang</a></h3>
                                 <p class="price">
-                                    <del></del>$143.00
+                                    <del></del>Rp. 40.000
                                 </p>
                             </div>
                             <!-- /.text-->
@@ -187,80 +160,7 @@
                         </div>
                         <!-- /.product-->
                     </div>
-                    <div class="item">
-                        <div class="product">
-                            <div class="flip-container">
-                                <div class="flipper">
-                                    <div class="front"><a href="detail.html"><img src="<?= base_url('assets/vendor_user/'); ?>img/product1.jpg" alt="" class="img-fluid"></a></div>
-                                    <div class="back"><a href="detail.html"><img src="<?= base_url('assets/vendor_user/'); ?>img/product1_2.jpg" alt="" class="img-fluid"></a></div>
-                                </div>
-                            </div><a href="detail.html" class="invisible"><img src="<?= base_url('assets/vendor_user/'); ?>img/product1.jpg" alt="" class="img-fluid"></a>
-                            <div class="text">
-                                <h3><a href="detail.html">Fur coat</a></h3>
-                                <p class="price">
-                                    <del></del>$143.00
-                                </p>
-                            </div>
-                            <!-- /.text-->
-                            <div class="ribbon gift">
-                                <div class="theribbon">GIFT</div>
-                                <div class="ribbon-background"></div>
-                            </div>
-                            <!-- /.ribbon-->
-                        </div>
-                        <!-- /.product-->
-                    </div>
-                    <div class="item">
-                        <div class="product">
-                            <div class="flip-container">
-                                <div class="flipper">
-                                    <div class="front"><a href="detail.html"><img src="<?= base_url('assets/vendor_user/'); ?>img/product2.jpg" alt="" class="img-fluid"></a></div>
-                                    <div class="back"><a href="detail.html"><img src="<?= base_url('assets/vendor_user/'); ?>img/product2_2.jpg" alt="" class="img-fluid"></a></div>
-                                </div>
-                            </div><a href="detail.html" class="invisible"><img src="<?= base_url('assets/vendor_user/'); ?>img/product2.jpg" alt="" class="img-fluid"></a>
-                            <div class="text">
-                                <h3><a href="detail.html">White Blouse Armani</a></h3>
-                                <p class="price">
-                                    <del>$280</del>$143.00
-                                </p>
-                            </div>
-                            <!-- /.text-->
-                            <div class="ribbon sale">
-                                <div class="theribbon">SALE</div>
-                                <div class="ribbon-background"></div>
-                            </div>
-                            <!-- /.ribbon-->
-                            <div class="ribbon new">
-                                <div class="theribbon">NEW</div>
-                                <div class="ribbon-background"></div>
-                            </div>
-                            <!-- /.ribbon-->
-                            <div class="ribbon gift">
-                                <div class="theribbon">GIFT</div>
-                                <div class="ribbon-background"></div>
-                            </div>
-                            <!-- /.ribbon-->
-                        </div>
-                        <!-- /.product-->
-                    </div>
-                    <div class="item">
-                        <div class="product">
-                            <div class="flip-container">
-                                <div class="flipper">
-                                    <div class="front"><a href="detail.html"><img src="<?= base_url('assets/vendor_user/'); ?>img/product3.jpg" alt="" class="img-fluid"></a></div>
-                                    <div class="back"><a href="detail.html"><img src="<?= base_url('assets/vendor_user/'); ?>img/product3_2.jpg" alt="" class="img-fluid"></a></div>
-                                </div>
-                            </div><a href="detail.html" class="invisible"><img src="<?= base_url('assets/vendor_user/'); ?>img/product3.jpg" alt="" class="img-fluid"></a>
-                            <div class="text">
-                                <h3><a href="detail.html">Black Blouse Versace</a></h3>
-                                <p class="price">
-                                    <del></del>$143.00
-                                </p>
-                            </div>
-                            <!-- /.text-->
-                        </div>
-                        <!-- /.product-->
-                    </div>
+
                     <!-- /.product-slider-->
                 </div>
                 <!-- /.container-->
