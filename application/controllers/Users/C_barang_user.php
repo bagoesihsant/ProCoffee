a7e2dee4cf7fd824e4ff06215b4b64edc597daed
lulungsatrioprayuda <alungsatrio12@gmail.com>
2020-12-22 23:55:28 +0700

    Header uncomment list barang adaptasi

--- application/controllers/Users/C_barang_user.php
+++ application/controllers/Users/C_barang_user.php
@@ -7,6 +7,7 @@ class C_barang_user extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        $this->load->model('M_barang', 'mbarang');
     }
 
     public function index()
--- application/views/user/v_list_barang.php
+++ application/views/user/v_list_barang.php
@@ -45,22 +45,41 @@
                             <div class="product">
                                 <div class="flip-container">
                                     <div class="flipper">
+
                                         <div class="front"><a href="detail.html"><img style="width: 250px; height:340px;" src="<?= base_url('assets/vendor_user/'); ?>img/kopi3.jpeg" alt="" class="img-fluid"></a></div>
                                         <!-- <div class="back"><a href="detail.html"><img src="<?= base_url('assets/vendor_user/'); ?>img/product1_3.jpg" alt="" class="img-fluid"></a></div> -->
                                     </div>
-                                </div><a href="<?= base_url('User/Detail') ?>" class="invisible"><img style="width: 250px; height:340px;" src="<?= base_url('assets/vendor_user/'); ?>img/kopi3.jpeg" alt="" class="img-fluid"></a>
+
+                                </div>
+                                <!-- Ini gambar  kalau di klik bakalan nganterin ke halaman detail sesuai dengan produk yang di klik-->
+                                <a href="<?= base_url('User/Detail') ?>" class="invisible"><img style="width: 250px; height:340px;" src="<?= base_url('assets/vendor_user/'); ?>img/kopi3.jpeg" alt="" class="img-fluid"></a>
+                                <!-- /Penutup Ini gambar  kalau di klik bakalan nganterin ke halaman detail sesuai dengan produk yang di klik-->
                                 <div class="text">
-                                    <h3><a href="<?= base_url('User/Detail') ?>">Fur coat with very but very very long name</a></h3>
+                                    <h3>
+                                        <!-- Ini judul barangnya juga kalau di klik bakalan nganterin ke halaman sesuai ama barang yang di klik -->
+                                        <a href="<?= base_url('User/Detail') ?>">Fur coat with very but very very long name</a>
+                                        <!-- /Penutup Ini judul barangnya juga kalau di klik bakalan nganterin ke halaman sesuai ama barang yang di klik -->
+                                    </h3>
                                     <p class="price">
+                                        <!-- Ini harga Barang -->
                                         <del></del>Rp. 55.000
+                                        <!-- Penutup Ini harga Barang -->
                                     </p>
-                                    <p class="buttons"><a href="<?= base_url('User/Detail') ?>" class="btn btn-outline-secondary">View detail</a><a href="basket.html" class="btn btn-primary"><i class="fa fa-shopping-cart"></i>Add to cart</a></p>
+                                    <p class="buttons">
+                                        <!-- pembuka tombol langsung tambah barang ke chart -->
+                                        <a href="<?= base_url('User/Detail') ?>" class="btn btn-outline-secondary">View detail</a>
+                                        <!-- Penutup tombol langsung tambah barang ke chart -->
+
+                                        <!-- pembuka tombol langsung ke detail barang-->
+                                        <a href="basket.html" class="btn btn-primary"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+                                        <!-- Penutup tombol langsung ke detail barang-->
+                                    </p>
                                 </div>
                                 <!-- /.text-->
                             </div>
-                            <!-- /.product            -->
+                            <!-- /.product -->
                         </div>
-                        <!-- sampe sini -->
+                        <!-- sampe sini penutup loopingnya -->
                     </div>
                     <!-- /.col-lg-9-->
                 </div>
