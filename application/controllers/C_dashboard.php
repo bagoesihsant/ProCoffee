23646b6078e7e664ad7d2921bb17e56158d20efc
Alfian Rochmatul Irman <alfianrochmatul77@gmail.com>
2020-12-28 22:44:57 +0700

    ngebenahin tampilan dashboard dan benahin badge

--- application/controllers/C_dashboard.php
+++ application/controllers/C_dashboard.php
@@ -26,16 +26,16 @@ class C_dashboard extends CI_Controller
     public function index()
     {
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
-        $this->data['total_satuan'] = $this->M_Satuan->total_rows();
-        $this->data['total_kategori'] = $this->M_Categories->total_rows();
-        $this->data['total_smenu'] = $this->m_sub_menu->total_rows();
-        $this->data['total_user'] = $this->M_user->total_rows();
-        $this->data['total_menu'] = $this->m_menu->total_rows();
-        $this->data['total_supplier'] = $this->M_supplier->total_rows();
-        $this->data['total_barang'] = $this->M_barang->total_rows();
-        $this->data['total_stockin'] = $this->M_stockin->total_rows();
-        $this->data['total_stockout'] = $this->M_stockOut->total_rows();
-        $this->data['total_role'] = $this->M_role->total_rows();
+        $data['total_satuan'] = $this->M_Satuan->total_rows();
+        $data['total_kategori'] = $this->M_Categories->total_rows();
+        $data['total_smenu'] = $this->m_sub_menu->total_rows();
+        $data['total_user'] = $this->M_user->total_rows();
+        $data['total_menu'] = $this->m_menu->total_rows();
+        $data['total_supplier'] = $this->M_supplier->total_rows();
+        $data['total_barang'] = $this->M_barang->total_rows();
+        $data['total_stockin'] = $this->M_stockin->total_rows();
+        $data['total_stockout'] = $this->M_stockOut->total_rows();
+        $data['total_role'] = $this->M_role->total_rows();
         $data['title'] = 'Dashboard';
 
         $data['cabang'] = $this->db->get('cabang')->result_array();
@@ -45,11 +45,12 @@ class C_dashboard extends CI_Controller
         $this->load->view('templates/admin/header', $data);
         $this->load->view('templates/admin/sidebar', $data);
         // if else
-        $data = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
-        if($data['kode_role'] == "RL0000000001"){
-            $this->load->view('admin/v_dashboard', $this->data);
+        $kdrole = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        if($kdrole['kode_role'] == "RL0000000001"){
+            $this->load->view('admin/v_dashboard', $data);
         }elseif($data['kode_role'] == "RL0000000002"){
-            $this->load->view('pemilik/v_dashboard_pemilik', $this->data);
+            $data['badge'] = "<i class='badge badge-danger'>Administrator</i>";
+            $this->load->view('pemilik/v_dashboard_pemilik', $data);
         }elseif($data['kode_role'] == "RL0000000003"){
             $this->load->view('pelanggan/v_pelanggan', $data);
         }elseif($data['kode_role'] == "RL0000000004"){
